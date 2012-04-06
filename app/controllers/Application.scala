1ce91da05aa32970cf906700723a527fa2f25f83
Vincent Jousse <vincent@jousse.org>
2012-04-06 16:59:59 +0200

    Handle format validation with play forms

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -16,17 +16,18 @@ object Application extends PygalaController {
   val codeForm = Form(
     tuple(
       "code" -> text,
-      "lang" -> text))
+      "lang" -> text) verifying ("Unsupported language to highlight", fields ⇒ fields match {
+        case (c, l) ⇒ env.supportedFormats.get(l.toLowerCase).isDefined
+      }))
 
   def highlight = Action { implicit request ⇒
     codeForm.bindFromRequest.fold(
-      formWithErrors ⇒ BadRequest("Oh, that's bad."),
-      value ⇒ env.supportedFormats.get(value._2.toLowerCase) match {
-        case Some(_) ⇒ env.parser.colorCode(value._1, value._2).fold(
-          error ⇒ BadRequest(error),
-          code ⇒ Ok(code.unsafePerformIO))
-        case None ⇒ BadRequest("Unsupported language to highlight: " + value._2)
-      })
+      formWithErrors ⇒ BadRequest({
+        for { error <- formWithErrors.errors } yield(error.message)
+      } mkString "\n"),
+      value ⇒ env.parser.colorCode(value._1, value._2).fold(
+        error ⇒ BadRequest(error),
+        code ⇒ Ok(code.unsafePerformIO)))
   }
 
   def lexers = Action {
